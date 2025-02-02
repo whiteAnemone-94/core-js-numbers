9800782081b41a915d6aaf026646c7f70c62c9fa
Olga Kamakhina <white.anemone.94@gmail.com>
2025-02-03 00:59:50 +0300

    feat: solve task getAngleBetweenVectors

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -105,8 +105,11 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const a = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const b = Math.sqrt(x2 ** 2 + y2 ** 2);
+  const angle = (x1 * x2 + y1 * y2) / (a * b);
+  return Math.acos(angle);
 }
 
 /**
