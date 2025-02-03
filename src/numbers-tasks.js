ab58ea112b80ca2605fc4341ce0f419c2cba4f1c
Olga Kamakhina <white.anemone.94@gmail.com>
2025-02-04 00:38:53 +0300

    feat: solve task isPrime

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -200,8 +200,16 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n === 2) {
+    return true;
+  }
+  for (let i = 2; i <= Math.trunc(Math.sqrt(n)); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
